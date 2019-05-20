aead85bec5e411c7e81c9bae2222d2377bac0073
Bruno Opsenica <bruno.opsenica@gmail.com>
2019-05-20 16:04:22 -0400

    Remove old lib

--- include/utils/Constants.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <math.h>
-
-namespace bae
-{
-constexpr float PI = M_PI;
-constexpr float HALF_PI = M_PI / 2;
-}  // namespace bae
--- src/utils/Geometric.cpp
+++
@@ -1,6 +0,0 @@
-#include "utils/Geometric.h"
-
-namespace bae
-{
-glm::vec3 crossAndNormalize(const glm::vec3 &u, const glm::vec3 &v) { return glm::normalize(glm::cross(u, v)); }
-}  // namespace bae
